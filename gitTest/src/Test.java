d2b79e7020bf957a31e58f637169cb6fe7fbb10a
Swei <Swei.J@outlook.com>
2022-01-24 15:21:11 +0800

    "Git测试"

---
+++ gitTest/src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: SweiPC
+ * Date: 2022-01-24
+ * Time: 15:19
+ */
+public class Test {
+    String name;
+    int age;
+
+    public Test(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
