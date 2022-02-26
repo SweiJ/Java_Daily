6777d0569aea6a6ad65965f5e642b9f61e1c1cb6
Swei <Swei.J@outlook.com>
2022-02-26 19:42:05 +0800

    servlet

---
+++ servlet02/Javaweb/src/com/powernode/javaweb/servlet/StudentServlet.java
@@ -0,0 +1,90 @@
+package com.powernode.javaweb.servlet;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: SweiPC
+ * Date: 2022-02-26
+ * Time: 15:03
+ */
+public class StudentServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response)
+            throws ServletException, IOException {
+        // jdbc
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/powernode?characterEncoding-utf8&useSSL=false&serverTimezone=UTC&rewriteBatchedStatements=true";
+            String user = "root";
+            String password = "jsw";
+            conn = DriverManager.getConnection(url, user, password);
+
+            String sql = "select * from student01";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while(rs.next()) {
+                String name = rs.getString("name");
+                String age = rs.getString("age");
+                out.print(name + " " + age + "<br>");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
