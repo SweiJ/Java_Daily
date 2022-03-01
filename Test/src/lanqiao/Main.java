6f9f2971967b759327dc863b0644f841b624b836
Swei <Swei.J@outlook.com>
2022-03-01 20:42:26 +0800

    课程

--- Test/src/lanqiao/Main.java
+++ Test/src/lanqiao/Main.java
@@ -11,8 +11,149 @@ import java.util.*;
  * Time: 18:34
  */
 public class Main {
-    // 数字游戏
     public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String numStr = bf.readLine();
+
+        String[] num = {"ling ", "yi ", "er ", "san ", "si ", "wu ", "liu ", "qi ", "ba ", "jiu "};
+        String[] wei = {"", "shi ", "bai ", "qian ", "wan ", "shi ", "bai ", "qian ", "yi ", "shi ", "bai "};
+        StringBuilder read = new StringBuilder();
+        int len = numStr.length();
+        // 0的判断
+        boolean b = true;
+        int i, j;
+
+        for (i = 0; i < numStr.length(); i++) {
+            // 第i位置的数字
+            for (j = 0; j <= 9; j++)
+                if (numStr.charAt(i) == j + '0')
+                    break;
+            // 数值位1的数处于当前数的最高位 也就是十亿位 or 十万位 or 十位
+            if ((numStr.length() + 2) % 4 == 0 && i == 0 && j == 1) {
+                read.append(wei[len- 1]);
+            // 判断当前数的位置如果不是最低位， 并且该数为0 也就是0的位置在中间
+            } else if (i != len - 1 && j == 0) {
+                // 判断如果连续存在两个0时，只增加一个ling
+                if (b) {
+                    read.append(num[j]);
+                    b = false;
+                }
+            // 如果最后一位的值为0
+            } else if (i == len - 1 && j == 0) {
+                // 如果最后一位前面也是0， 则删除前面的数
+                if (!b)
+                    read.append(read.substring(0, read.length() - 5));
+                // ling
+            } else {
+                read.append(num[j] + wei[numStr.length() - i - 1]);
+                b = true;
+            }
+        }
+        System.out.println(read);
+    }
+    public static void main5(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+        String[] arr = {"ling","yi","er","san","si","wu","liu","qi","ba","jiu","shi","bai","qian","wan","yi"};
+        int len = str.length();
+        switch (len) {
+            case 0 :
+                System.out.println(" ");
+                break;
+            case 1 :
+                System.out.println(arr[Integer.parseInt(str)]);
+                break;
+            case 2 :
+                if(str.charAt(1) == '0') {
+
+                    System.out.println(arr[Integer.valueOf(str.charAt(0))] + "shi");
+                } else {
+                    System.out.println(arr[Integer.valueOf(str.charAt(0))] + "shi" + arr[Integer.valueOf(str.charAt(1))]);
+                }
+                break;
+            case 3 :
+            case 4 :
+            case 5 :
+            case 6 :
+            case 7 :
+            case 8 :
+            case 9 :
+            case 10 :
+            case 11 :
+        }
+    }
+    // 矩阵乘法
+    public static void main4(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str[] = bf.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int m = Integer.parseInt(str[1]);
+        int[][] a = new int[n][n];
+        int[][] b = new int[n][n];
+        int[][] c = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            String input[] = bf.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                a[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            b[i][i] = 1;
+        }
+
+        while(m-- != 0) {
+            c = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        c[i][j] += b[i][k] * a[k][j];
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    b[i][j] = c[i][j];
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    // 杨辉三角
+    public static void main3(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        arr[0][0] = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if(j == 0) {
+                    arr[i][j] = 1;
+                    continue;
+                }
+                arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    // 进制转换
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String hex = Integer.toHexString(n);
+        System.out.println(hex.toUpperCase(Locale.ROOT));
+    }
+    // 数字游戏
+    public static void main1(String[] args) throws IOException {
         File file;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] arr = br.readLine().split(" ");
--- servlet02/Javaweb/src/com/powernode/javaweb/servlet/ServletDemo.java
+++ servlet02/Javaweb/src/com/powernode/javaweb/servlet/ServletDemo.java
@@ -1,11 +1,11 @@
 package com.powernode.javaweb.servlet;
 
+import jakarta.servlet.*;
 import jakarta.servlet.GenericServlet;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,7 +20,49 @@ public class ServletDemo extends GenericServlet {
 		System.out.print("ServletDemo.init");
 	}
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
 
+		ServletConfig config = this.getServletConfig();
+		// 该方法可以打印所有的servlet-name中的关键词
+		Enumeration<String> figParam = this.getInitParameterNames();
+		while(figParam.hasMoreElements()) {
+			String name = figParam.nextElement();
+			String value = config.getInitParameter(name);
+			out.print(name + "=" + value);
+		}
+
+		// 一个servlet对象 对应一个servletConfig对象， 100个servlet对象对应100个servletConfig对象
+		// 该方法主要存储所有的servlet对象 在一个webapp中共享一个servletContxt对象
+		// 在服务器启动的时候创建 关闭时销毁
+		ServletContext application = this.getServletContext();
+		Enumeration<String> textParam = application.getInitParameterNames();
+		while(textParam.hasMoreElements()) {
+			String name = textParam.nextElement();
+			String value = application.getInitParameter(name);
+			out.print(name + "=" + value + "<br>");
+		}
+
+		// 获取应用的根 此项目的根为/xmm
+		String path = application.getContextPath();
+		out.print(path + "<br>");
+
+		// 加上"/"表示从根开始, 打印的是web.xml的路径
+		String realPath = application.getRealPath("/");
+		out.print(realPath + "<br>");
+
+		// 加上"/"和不加"/"都可以
+		String realPath1 = application.getRealPath("student.html");
+		String realPath2 = application.getRealPath("/student.html");
+		out.print(realPath1 + "<br>" + realPath2);
+
+		// catalina.2022-03-01.log 服务器端的java程序运行的控制台信息
+		// localhost.2022-03-01.log ServletContext对象的log方法记录的日志信息存储到这个文件中
+		// localhost_access_log.2022-03-01.txt 访问日志
+		int a = 10;
+		if(a < 18) {
+			application.log("当前值小于正常值", new Exception("存在一个错误！"));
+		}
     }
 }
