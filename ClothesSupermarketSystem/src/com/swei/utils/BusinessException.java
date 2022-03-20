e6413571053396b998b3e4b30294c71f695a1b93
SweiGithub <Swei.J@outlook.com>
2022-03-20 22:21:40 +0800

    衣服

--- ClothesSupermarketSystem/src/com/swei/utils/BusinessException.java
+++ ClothesSupermarketSystem/src/com/swei/utils/BusinessException.java
@@ -11,7 +11,7 @@ public class BusinessException extends RuntimeException {
     public BusinessException() {
         super();
     }
-    
+
 
     public BusinessException(String message) {
         super(message);
