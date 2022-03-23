86d1e49d300e7773020142d934c83c5e84060c0a
SweiGithub <Swei.J@outlook.com>
2022-03-23 16:50:12 +0800

    Test

--- Test/src/leecode/Main.java
+++ Test/src/leecode/Main.java
@@ -2,6 +2,8 @@ package leecode;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -11,8 +13,67 @@ import java.util.Scanner;
  * Date: 2022-03-21
  * Time: 9:37
  */
+class StoreValue {
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int begin = 0;
+        int end = 0;
+        StringBuilder sb = new StringBuilder(" ");
+        for (int i = 0; i < str.length(); i++) {
+            char s = str.charAt(i);
+            if(s > '0' && s < '9') {
+                begin = i;
+                while (i < str.length() && str.charAt(i) > '0' && str.charAt(i) < '9') {
+                    i++;
+                }
+                end = i;
+                if(end == str.length() - 1) {
+                    end++;
+                }
+                String t = str.substring(begin, end);
+
+                if (sb.length() < t.length()) {
+                    sb.delete(0, sb.length());
+                    sb.append(t);
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+    public static int MoreThanHalfNum_Solution(int [] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if(array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                 }
+            }
+        }
+        return array[array.length / 2];
+    }
+    public static void main4(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
