56f2160fe000913001e0058f995b2cea8f169a99
SweiGithub <Swei.J@outlook.com>
2022-06-05 21:55:01 +0800

    homework

--- ServletTest/src/main/java/com/swei/HelloServlet.java
+++ ServletTest/src/main/java/com/swei/HelloServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,8 +20,27 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf8");
+
         PrintWriter writer = resp.getWriter();
-        writer.print("Hello Servlet!");
+        writer.write(String.valueOf(req.getRequestURL()));
+        writer.write("<hr/>");
+        writer.write(req.getMethod());
+        writer.write("<hr/>");
+        writer.write(req.getParameter("name"));
+        writer.write("<hr/>");
+        writer.write(req.getQueryString());
+        writer.write("<hr/>");
+        writer.write(req.getContextPath());
+        writer.write("<hr/>");
+        writer.println(req.getHeaderNames());
+
+        Enumeration<String> names = req.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String headName = names.nextElement();
+            String header = req.getHeader(headName);
+            writer.write(headName + ":" + header + "<hr/>");
+        }
     }
 
     @Override
