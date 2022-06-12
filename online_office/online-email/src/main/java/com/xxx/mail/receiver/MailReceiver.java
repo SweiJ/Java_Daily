11f44ab1d8e43857b3a99b6fe8461803de4eef6d
SweiGithub <Swei.J@outlook.com>
2022-06-12 21:18:12 +0800

    office-email

--- online_office/online-email/src/main/java/com/xxx/mail/receiver/MailReceiver.java
+++ online_office/online-email/src/main/java/com/xxx/mail/receiver/MailReceiver.java
@@ -1,50 +1,50 @@
-//package com.xxx.mail.receiver;
-//
-//import com.rabbitmq.client.Channel;
-//import com.xxx.server.pojo.Employee;
-//import com.xxx.server.pojo.MailConstants;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.amqp.rabbit.annotation.RabbitListener;
-//import org.springframework.amqp.support.AmqpHeaders;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.mail.MailProperties;
-//import org.springframework.data.redis.core.HashOperations;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.MimeMessageHelper;
-//import org.springframework.messaging.Message;
-//import org.springframework.messaging.MessageHeaders;
-//import org.springframework.stereotype.Component;
-//import org.thymeleaf.TemplateEngine;
-//import org.thymeleaf.context.Context;
-//
-//import javax.mail.MessagingException;
-//import javax.mail.internet.MimeMessage;
-//import java.io.IOException;
-//import java.util.Date;
-//
-///**
-// * 消息接收者
-// *
-// * @author zhanglishen
-// */
-//@Component
-//public class MailReceiver {
-//    public static final Logger logger = LoggerFactory.getLogger(MailReceiver.class);
-//
-//    @Autowired
-//    private JavaMailSender javaMailSender;
-//    @Autowired
-//    private MailProperties mailProperties;
-//    @Autowired
-//    private TemplateEngine templateEngine;
-//    @Autowired
-//    private RedisTemplate redisTemplate;
-//
-//    /**
-//     * 邮件发送
-//     */
+package com.xxx.mail.receiver;
+
+import com.rabbitmq.client.Channel;
+import com.xxx.server.pojo.Employee;
+import com.xxx.server.pojo.MailConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * 消息接收者
+ *
+ * @author zhanglishen
+ */
+@Component
+public class MailReceiver {
+    public static final Logger logger = LoggerFactory.getLogger(MailReceiver.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Autowired
+    private MailProperties mailProperties;
+    @Autowired
+    private TemplateEngine templateEngine;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 邮件发送
+     */
 //    @RabbitListener(queues = MailConstants.MAIL_QUEUE_NAME)
 //    public void handler(Message message, Channel channel) {
 //
@@ -100,28 +100,26 @@
 //            logger.error("MailReceiver + 邮件发送失败========{}", e.getMessage());
 //        }
 //    }
-//
-//    /*
-//    @RabbitListener(queues = MailConstants.MAIL_QUEUE_NAME)
-//    public void handler(Employee employee) {
-//        MimeMessage message = javaMailSender.createMimeMessage();
-//        MimeMessageHelper helper = new MimeMessageHelper(message);
-//        try {
-//            helper.setFrom(mailProperties.getUsername());
-//            helper.setTo(employee.getEmail());
-//            helper.setSubject("入职邮件");
-//            helper.setSentDate(new Date());
-//            Context context = new Context();
-//            context.setVariable("name", employee.getName());
-//            context.setVariable("posName", employee.getPosition().getName());
-//            context.setVariable("joblevelName", employee.getJoblevel().getName());
-//            context.setVariable("departmentName", employee.getDepartment().getName());
-//            String mail = templateEngine.process("mail", context);
-//            helper.setText(mail, true);
-//            javaMailSender.send(message);
-//        } catch (MessagingException e) {
-//            logger.error("MailReceiver + 邮件发送失败========{}", e.getMessage());
-//        }
-//    }
-//     */
-//}
+
+    @RabbitListener(queues = MailConstants.MAIL_QUEUE_NAME)
+    public void handler(Employee employee) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        try {
+            helper.setFrom(mailProperties.getUsername());
+            helper.setTo(employee.getEmail());
+            helper.setSubject("入职邮件");
+            helper.setSentDate(new Date());
+            Context context = new Context();
+            context.setVariable("name", employee.getName());
+            context.setVariable("posName", employee.getPosition().getName());
+            context.setVariable("joblevelName", employee.getJoblevel().getName());
+            context.setVariable("departmentName", employee.getDepartment().getName());
+            String mail = templateEngine.process("mail", context);
+            helper.setText(mail, true);
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            logger.error("MailReceiver + 邮件发送失败========{}", e.getMessage());
+        }
+    }
+}
