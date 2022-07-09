ac41b2dbcb49ffa27e7d9db4cc288915ff4f25a7
SweiGithub <Swei.J@outlook.com>
2022-07-09 19:22:33 +0800

    model

---
+++ shop/zf-parent/model/src/main/java/com/swei/entity/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.swei.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class BaseEntity implements Serializable {
+
+    private Long id;
+    private Date createTime;
+    private Date updateTime;
+    private Integer isDeleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
