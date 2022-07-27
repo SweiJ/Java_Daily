d12b0d85a93ee6f451b4aa865277546667f9589c
SweiGithub <Swei.J@outlook.com>
2022-07-27 21:59:48 +0800

    blog-sys-init

---
+++ ssm-blogs-system/src/main/java/com/swei/SpringBootBlogApplication.java
@@ -0,0 +1,16 @@
+package com.swei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author SweiPC
+ */
+@SpringBootApplication
+public class SpringBootBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlogApplication.class);
+    }
+}
---
+++ ssm-blogs-system/src/test/java/com/swei/SpringBootBlogApplication.java
@@ -0,0 +1,14 @@
+package com.swei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit test for simple SpringBootBlogApplication.
+ */
+@SpringBootTest
+public class SpringBootBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBlogApplication.class);
+    }
+}
