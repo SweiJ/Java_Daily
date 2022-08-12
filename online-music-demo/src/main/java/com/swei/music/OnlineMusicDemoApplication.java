8ed665587fc65b7d7308736e22deec607c3ac1f8
SweiGithub <Swei.J@outlook.com>
2022-08-12 21:18:14 +0800

    online-music-demo-静态资源

---
+++ online-music-demo/src/main/java/com/swei/music/OnlineMusicDemoApplication.java
@@ -0,0 +1,14 @@
+package com.swei.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class OnlineMusicDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMusicDemoApplication.class, args);
+    }
+
+}
