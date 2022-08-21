7677f62ea967301fb4abddc0d59125f7b7a0e585
SweiGithub <Swei.J@outlook.com>
2022-08-21 18:27:36 +0800

    online-meal-sec-master 前端资源

---
+++ order-meal-sec-master/src/test/java/com/swei/ordermeal/OrderMealMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swei.ordermeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMealMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
