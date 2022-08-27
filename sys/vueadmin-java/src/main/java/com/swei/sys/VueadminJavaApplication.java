3670c30958f678a6412547741f47354f866278ca
SweiGithub <Swei.J@outlook.com>
2022-08-27 22:29:49 +0800

    vueadmin

---
+++ sys/vueadmin-java/src/main/java/com/swei/sys/VueadminJavaApplication.java
@@ -0,0 +1,13 @@
+package com.swei.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueadminJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueadminJavaApplication.class, args);
+    }
+
+}
---
+++ sys/vueadmin-java/src/test/java/com/swei/sys/VueadminJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swei.sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueadminJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
