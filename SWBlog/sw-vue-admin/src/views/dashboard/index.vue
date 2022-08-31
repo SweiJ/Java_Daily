b205b5db41302531f74f45903451a8eed4789b35
SweiGithub <Swei.J@outlook.com>
2022-08-31 12:04:11 +0800

    sw-blog-admin-前端管理后台资源

---
+++ SWBlog/sw-vue-admin/src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      <h1 style="color: rgb(81,193,142);">SW博客管理系统</h1>
+    </div>
+    <div>
+      <h2>博客地址: <a href="www.swxpz.cn">www.swxpz.cn</a></h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 100px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
