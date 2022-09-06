4e03fc4bae410d4a7b79d9ac8d0709f101239a95
SweiGithub <Swei.J@outlook.com>
2022-09-06 16:18:32 +0800

    online-meal-sec-master

--- order-meal-sec-master/src/main/java/com/swei/ordermeal/config/security/WebSecurityConfig.java
+++ order-meal-sec-master/src/main/java/com/swei/ordermeal/config/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import com.swei.ordermeal.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -37,6 +38,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private EmployeeService employeeService;
 
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
